64ee823b8010254247f6e4a15994b419f84f7b05
Andika Saputra <acyandika2@gmail.com>
2025-02-11 16:52:27 +0800

    fiture delete done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.get("/api", (req, res) => {
   res.send("Hello Kamu");
 });
@@ -15,6 +17,34 @@ app.get("/api", (req, res) => {
 app.get("/products", async (req, res) => {
   const product = await prisma.product.findMany();
   res.send(product);
+});
+
+app.post("/products", async (req, res) => {
+  const newProductData = req.body;
+
+  const products = await prisma.product.create({
+    data: {
+      name: newProductData.name,
+      description: newProductData.description,
+      price: newProductData.price,
+      image: newProductData.image,
+    },
+  });
+  res.send({
+    data: products,
+    message: "create products success",
+  });
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const productId = req.params.id;
+  await prisma.product.delete({
+    where: {
+      id: parseInt(productId),
+    },
+  });
+
+  res.send("product deleted");
 });
 
 app.listen(PORT, () => {
